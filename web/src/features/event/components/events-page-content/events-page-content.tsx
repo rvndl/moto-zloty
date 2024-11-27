8005968162188306f8cf126a83ee986e9773d4ef
rvndl <rivendelltolump@gmail.com>
2024-11-27 15:36:59 +0100

    feat: pre-rendering, adsense, sitemap

--- web/src/features/event/components/events-page-content/events-page-content.tsx
+++ web/src/features/event/components/events-page-content/events-page-content.tsx
@@ -3,10 +3,17 @@ import { useEventsQuery } from "@features/event/api";
 import { EventsList } from "./events-list";
 import { EventMapMarker } from "../event-map-marker";
 import { useIsMobile } from "@hooks/use-is-mobile";
+import { useEffect } from "react";
 
 const EventsPageContent = () => {
   const { data: events, isLoading } = useEventsQuery();
   const isMobile = useIsMobile();
+
+  useEffect(() => {
+    if (events?.length) {
+      document.dispatchEvent(new Event("pre-render"));
+    }
+  }, [events]);
 
   return (
     <div className="flex flex-col w-full h-full gap-2">
--- web/vite.config.ts
+++ web/vite.config.ts
@@ -3,6 +3,10 @@ import react from "@vitejs/plugin-react-swc";
 import tsconfigPaths from "vite-tsconfig-paths";
 import svgr from "vite-plugin-svgr";
 import Unfonts from "unplugin-fonts/vite";
+import adsense from "./plugins/adsense";
+import robots from "./plugins/robots";
+import Sitemap from "vite-plugin-sitemap";
+import prerender from "@prerenderer/rollup-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,6 +17,24 @@ export default defineConfig({
     Unfonts({
       custom: {
         families: [{ name: "Geist", src: "./src/assets/fonts/*.woff2" }],
+      },
+    }),
+    adsense(),
+    robots(),
+    Sitemap({ hostname: "https://moto-zloty.pl" }),
+    prerender({
+      routes: ["/"],
+      renderer: "@prerenderer/renderer-puppeteer",
+      rendererOptions: {
+        renderAfterDocumentEvent: "pre-render",
+      },
+      postProcess(renderedRoute) {
+        renderedRoute.html = renderedRoute.html
+          .replace(/http:/gi, "https:")
+          .replace(
+            /(https:\/\/)?(localhost|127\.0\.0\.1):\d*/gi,
+            process.env.CI_ENVIRONMENT_URL || ""
+          );
       },
     }),
   ],
