e0fcf355d7d0b52ffc75b67baa82c4331fd11949
rvndl <rivendelltolump@gmail.com>
2024-12-07 21:51:06 +0100

    fix: remove height restriction from textarea

--- web/src/components/textarea.tsx
+++ web/src/components/textarea.tsx
@@ -19,7 +19,7 @@ const Textarea = ({
   const id = useId();
 
   const classes = clsx(
-    "flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors placeholder:text-muted focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
+    "flex w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors placeholder:text-muted focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
     className
   );
 
