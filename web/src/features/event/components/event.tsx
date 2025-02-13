cdc6b87779b4549e59cb97662c038fe9c9931938
rvndl <rivendelltolump@gmail.com>
2025-02-13 15:35:50 +0100

    feat: add event name to the card

--- web/src/features/event/components/event.tsx
+++ web/src/features/event/components/event.tsx
@@ -6,7 +6,7 @@ import { pl } from "date-fns/locale";
 import { Badge } from "@components";
 import clsx from "clsx";
 import { getEventStatus } from "@utils/event";
-import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 type EventSize = "normal" | "small";
 
@@ -16,8 +16,6 @@ interface Props {
 }
 
 const Event = ({ event, size = "normal" }: Props) => {
-  const router = useRouter();
-
   const { isOngoing, isPast } = useMemo(() => getEventStatus(event), [event]);
 
   const distance = formatDistance(
@@ -26,31 +24,31 @@ const Event = ({ event, size = "normal" }: Props) => {
     { locale: pl, addSuffix: true }
   );
 
-  const handleOnClick = () => {
-    router.push(`/event/${event.id}`);
-  };
-
   return (
-    <div
-      className={clsx(
-        "relative overflow-hidden border rounded-lg shadow-sm cursor-pointer h-20 md:h-32 white shrink-0 snap-start hover:-translate-y-1 transition-transform z-10",
-        size === "normal" ? "w-32 md:w-52" : "w-36 md:w-44",
-        isPast && "opacity-50"
-      )}
-      onClick={handleOnClick}
-    >
-      <Badge
-        variant={isOngoing ? "danger" : "secondary"}
-        className="absolute z-10 top-1 left-1"
+    <Link href={`/event/${event.id}`} rel="dofollow">
+      <div
+        className={clsx(
+          "relative overflow-hidden border rounded-lg shadow-sm cursor-pointer h-20 md:h-32 white shrink-0 snap-start hover:-translate-y-1 transition-transform z-10 group select-none",
+          size === "normal" ? "w-32 md:w-52" : "w-36 md:w-44",
+          isPast && "opacity-50"
+        )}
       >
-        {isOngoing ? "W trakcie" : distance}
-      </Badge>
-      <img
-        src={getFilePath(event.banner_small_id ?? event.banner_id)}
-        className="object-cover w-full h-full transition rounded-md z-200 hover:scale-105"
-        alt="banner"
-      />
-    </div>
+        <p className="pointer-events-none max-w-fit translate-y-1 truncate font-normal absolute bottom-1 left-1 right-1 z-10 px-2 py-0.5 leading-none transition border border-muted/50 text-white text-sm bg-white/30 opacity-0 rounded-md group-hover:opacity-100 group-hover:translate-y-0 backdrop-blur-md">
+          {event.name}
+        </p>
+        <Badge
+          variant={isOngoing ? "danger" : "secondary"}
+          className="absolute z-10 top-1 left-1"
+        >
+          {isOngoing ? "W trakcie" : distance}
+        </Badge>
+        <img
+          src={getFilePath(event.banner_small_id ?? event.banner_id)}
+          className="object-cover w-full h-full transition rounded-md z-200 hover:scale-105"
+          alt={event.name}
+        />
+      </div>
+    </Link>
   );
 };
 
