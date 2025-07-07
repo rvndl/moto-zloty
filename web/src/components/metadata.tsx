243a7b020f541c9fdc4b145891f1b409dc377878
rvndl <filipliwinski6@gmail.com>
2025-07-07 18:02:17 +0200

    feat: structured data

--- web/src/components/metadata.tsx
+++ web/src/components/metadata.tsx
@@ -2,18 +2,46 @@ import { stripHtml } from "string-strip-html";
 import Head from "next/head";
 import { truncate } from "lodash";
 
+interface StructuredData {
+  headline?: string;
+  image?: string;
+  datePublished?: string;
+  author?: {
+    name?: string;
+    url: string;
+  };
+}
+
 interface Props {
   title: string;
   description?: string;
   canonical?: string;
+  structuredData?: StructuredData;
 }
 
-const Metadata = ({ title, description, canonical }: Props) => {
+const Metadata = ({ title, description, canonical, structuredData }: Props) => {
   const siteUrl = process.env.NEXT_PUBLIC_PUBLIC_URL;
   const strippedDescription = truncate(stripHtml(description ?? "").result, {
     length: 160,
     omission: "...",
   });
+
+  const structuredDataJson =
+    structuredData &&
+    JSON.stringify({
+      "@context": "https://schema.org",
+      "@type": "NewsArticle",
+      headline: structuredData.headline,
+      image: [structuredData.image],
+      datePublished: structuredData.datePublished,
+      author: [
+        {
+          "@type": "Person",
+          name: structuredData.author.name,
+          url: structuredData.author.url,
+        },
+      ],
+    });
 
   return (
     <Head>
@@ -30,6 +58,11 @@ const Metadata = ({ title, description, canonical }: Props) => {
         <>
           <link rel="canonical" href={siteUrl + canonical} />
           <meta property="og:url" content={siteUrl + canonical} />
+        </>
+      )}
+      {structuredData && (
+        <>
+          <script type="application/ld+json">{structuredDataJson}</script>
         </>
       )}
     </Head>
