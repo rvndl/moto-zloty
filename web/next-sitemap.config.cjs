fe1b52c4bc9eb670004fe4c47805aeb0becb74ad
rvndl <filipliwinski6@gmail.com>
2025-08-05 17:33:09 +0200

    fix: exclude event creation page from the sitemap

--- web/next-sitemap.config.cjs
+++ web/next-sitemap.config.cjs
@@ -2,7 +2,7 @@
 module.exports = {
   siteUrl: process.env.NEXT_PUBLIC_PUBLIC_URL,
   generateRobotsTxt: true,
-  exclude: ["/server-sitemap.xml", "/moderation"],
+  exclude: ["/server-sitemap.xml", "/moderation", "/wydarzenie/stworz"],
   robotsTxtOptions: {
     policies: [
       { userAgent: "*", allow: "/" },
