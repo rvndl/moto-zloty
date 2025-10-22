6639a1de3fa9679391f8dcfce003ed1bf366f58d
rvndl <filipliwinski6@gmail.com>
2025-10-22 20:11:39 +0200

    chore: remove unused image scrapper

--- web/src/features/event/pages/map-page.tsx
+++ web/src/features/event/pages/map-page.tsx
@@ -8,7 +8,6 @@ const MapPage = () => {
       <Metadata
         title="Moto Zloty - Wszystkie zloty, imprezy motocyklowe w Polsce!"
         description="Wszystkie zloty motocyklowe, wydarzenia i imprezy w Polsce w jednym miejscu! Sprawdź, co dzieje się w Twojej okolicy i dołącz już dziś!"
-        canonical=""
       />
       <Page title="Mapa wydarzeń">
         <MapPageContent />
